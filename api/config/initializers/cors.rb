e0082ef5b564b7de2e5161ee5304d563c53ce1ef
Kan-prog <forwkan@gmail.com>
2021-04-23 17:23:19 +0900

    微修正

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
+      # Dockerでfront:8080ポートにforntのURLを設定しているため。
       origins 'http://front:8080'
   
       resource '*',
