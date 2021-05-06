09f6b0c06711a996ebc9a022e64a11d21e13ee8d
a <mozu113000@gmail.com>
2021-05-06 10:55:57 +0900

    model関連付け、validatesとindex追加

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   belongs_to :language
+  has_many :memos
+  has_many :diaries
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
