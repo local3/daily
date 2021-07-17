f8b8a498d69675db5df321a8bd6434ee2fef87a6
Kan-prog <pekkanseig8@icloud.com>
2021-07-18 04:07:28 +0900

    typescript入れたけどwarnは消えてない

--- front/src/components/AlertMsg.tsx
+++ front/src/components/AlertMsg.tsx
@@ -1,8 +1,15 @@
 import React, { useContext } from 'react'
 import { AlertContext } from '../store/AlertProvider'
-
-const AlertMsg = () => {
-	const alertState = useContext(AlertContext)
+type AleartContext = {
+	msg: string
+	status: number
+	className: string
+}
+interface Props {
+	
+}
+const AlertMsg: React.FC<Props> = () => {
+	const alertState: AleartContext = useContext(AlertContext)
 	return(
 		<div className={alertState.className}>
     	{alertState.msg}
