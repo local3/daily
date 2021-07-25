815be27d0d929ba4aa56ffb9aa49301adac51e0a
a <mozu113000@gmail.com>
2021-07-25 16:49:29 +0900

    DiaryTipデザイン完成

--- front/src/components/DiaryTip.tsx
+++ front/src/components/DiaryTip.tsx
@@ -1,25 +1,42 @@
-import React, { useState, useEffect, useContext } from "react"
+import { useState, useEffect, useContext } from "react"
 import axios from "axios"
 import { DateContext } from "../store/DateProvider"
+import { Card, CardContent, Typography }  from '@material-ui/core'
+import { useCalendarStyles } from "../styles/js/Calendar"
 
 const DiaryTip = () => {
+  // DateProviderからカレンダーで選択中の日付を取得
   const dateContext = useContext(DateContext)
-  const [diaryContent, setDiaryContetnt] = useState('未記入')
-  axios.get(`/diaries/${dateContext.date}`)
+  const date = dateContext.date.replace(/(\d+)-(\d+)-(\d+)/, "$1年$2月$3日")
+  const [diaryContent, setDiaryContetnt] = useState('')
+  const calendarClasses = useCalendarStyles()
+
+  // 日付に変更があった場合、日本語の日記内容を取得
+  const updateDiaryTipEffect = () => {
+    axios.get(`/diaries/${date}`)
     .then(res => {
       console.log(res)
       if(res.data.diary){
-        setDiaryContetnt(res.data.diary.jaContent)
+        setDiaryContetnt(res.data.diary.ja_content)
+        console.log(diaryContent)
+      }else{
+        setDiaryContetnt('未記入')
       }
     })
+  }
+
+  useEffect(updateDiaryTipEffect, [date])
+  
   return(
     <>
-      { diaryContent &&
-        <div>
-          {dateContext.date}
-          {diaryContent}
-        </div>
-      }
+      <Card className={calendarClasses.CardStyles}>
+        <CardContent>
+            <div>
+              <Typography variant="h5" gutterBottom>{date}</Typography>
+              <Typography>{diaryContent}</Typography>
+            </div>
+        </CardContent>
+      </Card>
     </>
   )
 }
