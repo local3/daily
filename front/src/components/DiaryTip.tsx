dcbbd6074321b4965f87e3b006085cbbe465dbf5
a <mozu113000@gmail.com>
2021-07-29 17:05:19 +0900

    DiaryTipデザイン

--- front/src/components/DiaryTip.tsx
+++ front/src/components/DiaryTip.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from "react"
 import axios from "axios"
 import { DateContext } from "../store/DateProvider"
-import { Card, CardContent, Typography }  from '@material-ui/core'
+import { Drawer, Typography }  from '@material-ui/core'
 import { useCalendarStyles } from "../styles/js/Calendar"
 
 const DiaryTip = () => {
@@ -11,6 +11,11 @@ const DiaryTip = () => {
   const displayDate = date.replace(/(\d+)-(\d+)-(\d+)/, "$1年$2月$3日")
   const [diaryContent, setDiaryContetnt] = useState('')
   const calendarClasses = useCalendarStyles()
+  const [isTipOpen, setIsTipOpen] = useState(true)
+  
+  const toggleDrawer = (event: React.MouseEvent,) => {
+    setIsTipOpen(false)
+  };
 
   // 日付に変更があった場合、日本語の日記内容を取得
   const updateDiaryTipEffect = () => {
@@ -23,26 +28,34 @@ const DiaryTip = () => {
         }else{
           setDiaryContetnt(res.data.diary.ja_content)
         }
-        console.log(diaryContent)
+        setIsTipOpen(true)
+        console.log(isTipOpen)
+        // クラスを追加して、CSSを上書きする
+        const element = document.querySelector('div[class=MuiBackdrop-root]')
+        element && element.classList.add(calendarClasses.DrawerStyle)
       }else{
-        setDiaryContetnt('未記入')
+        setDiaryContetnt('')
       }
     })
   }
 
   useEffect(updateDiaryTipEffect, [date])
-  
-  return(
-    <>
-      <Card className={calendarClasses.CardStyles}>
-        <CardContent>
-            <div>
-              <Typography variant="h5" gutterBottom>{displayDate}</Typography>
-              <Typography>{diaryContent}</Typography>
-            </div>
-        </CardContent>
-      </Card>
-    </>
-  )
+
+  // 日記未記入の場合は表示しない
+  if (diaryContent) {
+    return(
+      <>
+        <Drawer anchor={'bottom'} open={isTipOpen} onClose={toggleDrawer} className={calendarClasses.DrawerStyle}
+          classes={{ paperAnchorBottom: calendarClasses.TipPaperStyle }}>
+            <Typography variant="h6" classes={{ h6: calendarClasses.TipDateStyle}} gutterBottom>{displayDate}</Typography>
+            <Typography>{diaryContent}</Typography>
+        </Drawer>
+      </>
+    )
+  }else{
+    return(
+      null
+    )
+  }
 }
 export default DiaryTip
