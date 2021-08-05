327f8a83101132c3bb1a0d11ad8053a361d13ae1
Kan-prog <pekkanseig8@icloud.com>
2021-08-05 18:40:56 +0900

    solved issue #75

--- front/src/components/layouts/Footer.tsx
+++ front/src/components/layouts/Footer.tsx
@@ -1,28 +1,28 @@
-import React,{ useState, useContext, useEffect }  from 'react'
+import React,{ useState, useContext }  from 'react'
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core'
 import { 
   CalendarToday, FileCopy, Create, LocalOffer, MoreHoriz,
   CalendarTodayOutlined, FileCopyOutlined, CreateOutlined, LocalOfferOutlined, MoreHorizOutlined,
   Check
 } from '@material-ui/icons'
-import layoutStyles, { useLayoutStyles } from '../../styles/js/layout'
+import { useLayoutStyles } from '../../styles/js/layout'
 import { useHistory } from 'react-router-dom'
 import { DateContext } from '../../store/DateProvider'
 import { today } from '../../utils/Date'
 import { DiaryFormContentContext } from '../../store/DiaryFormContentProvider'
 import EtcDrawer from './EtcDrawer'
+import { CALENDAR_URL, MEMOS_URL, CLEAR_DIARY_URL, DICTIONARIES_URL } from '../../utils/paths'
 
 const Footer = () => {
   // console.log("fppter")
   const history = useHistory();
   const layoutClasses = useLayoutStyles()
   // 保存ボタンを押した時に保存フラグを立てる関数
-  const { submitFlag, changeSubmitFlag } = useContext(DiaryFormContentContext)
+  const { changeSubmitFlag } = useContext(DiaryFormContentContext)
   // 各ボタンを押した時にカレンダーで選択した日にちの日記作成画面に遷移できるようにするため
   const { globalDate } = useContext(DateContext)
   // /calendarにいるときは、選択した日時にの日記作成画面に遷移するが、そうでないときは、今日の日にちの日記作成画面に遷移する
   const redirectDate = history.location.pathname === '/calendar' ? globalDate : today
-  const [value, setValue] = useState(0);
   const [isOpenEtcDrawer, setIsOpenEtcDrawer] = useState(false)
 
   // フッターのボタンを押したときのページ遷移のアクション
@@ -37,18 +37,26 @@ const Footer = () => {
   }
   // その他を押した時にDrawer表示を切り替える
   const toggleEtcDrawer = () => {
-    console.log(isOpenEtcDrawer)
     setIsOpenEtcDrawer(!isOpenEtcDrawer)
   }
-  // useEffect(()=>{console.log("aaa")}, [submitFlag])
+  // FooterのBottomNavigationに使うボタン情報
   const navActions = [
-    {label: "カレンダー", icon: <CalendarToday />, iconSelected: <CalendarTodayOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", action: ()=>redirectAction('/calendar')},
-    {label: "メモ", icon: <FileCopy />, iconSelected: <FileCopyOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", action: ()=>redirectAction('/memos')},
-    {label: "書く", icon: <Create />, iconSelected: <CreateOutlined />, className: "footerCenterButton", selectedClassName: "selectedFooterCenterButton", action: ()=>redirectAction(`/diary/${redirectDate}`)},
-    {label: "保存", icon: <Check />, iconSelected: <Check />, className: "footerCenterButton", selectedClassName: "selectedFooterCenterButton", action: saveAction},
-    {label: "My辞書", icon: <LocalOffer />, iconSelected: <LocalOfferOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", action: ()=>redirectAction('/dictionary')},
-    {label: "その他", icon: <MoreHoriz />, iconSelected: <MoreHorizOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", action: toggleEtcDrawer}
+    {id: 0, label: "カレンダー", icon: <CalendarToday />, iconSelected: <CalendarTodayOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton",　url: CALENDAR_URL, action: ()=>redirectAction(CALENDAR_URL)},
+    {id: 1, label: "メモ", icon: <FileCopy />, iconSelected: <FileCopyOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", url: MEMOS_URL, action: ()=>redirectAction(MEMOS_URL)},
+    {id: 2, label: "書く", icon: <Create />, iconSelected: <CreateOutlined />, className: "footerCenterButton", selectedClassName: "selectedFooterCenterButton", url: `${CLEAR_DIARY_URL}/${redirectDate}`, action: ()=>redirectAction(`${CLEAR_DIARY_URL}/${redirectDate}`)},
+    {id: 3, label: "保存", icon: <Check />, iconSelected: <Check />, className: "footerCenterButton", selectedClassName: "selectedFooterCenterButton", url: null, action: saveAction},
+    {id: 4, label: "My辞書", icon: <LocalOffer />, iconSelected: <LocalOfferOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", url: DICTIONARIES_URL, action: ()=>redirectAction(DICTIONARIES_URL)},
+    {id: 5, label: "その他", icon: <MoreHoriz />, iconSelected: <MoreHorizOutlined />, className: "footerButton", selectedClassName: "selectedFooterButton", url: null, action: toggleEtcDrawer}
   ]
+  // URLによって選択中のnavActionの番号を決定する
+  const getInitFooterValue = () => {
+    const selectedNavAction = navActions.find(navAction => navAction.url === history.location.pathname)
+    return selectedNavAction ? selectedNavAction.id : 0
+  }
+
+  const initFooterValue = getInitFooterValue
+  // 選択中のnavAcitonの番号
+  const [value, setValue] = useState(initFooterValue)
   return(
     <>
       <BottomNavigation
