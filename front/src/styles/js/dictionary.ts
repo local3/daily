8e1dd0e02d0c82765345441864dd1d12329bc7f5
Kan-prog <pekkanseig8@icloud.com>
2021-08-05 17:00:34 +0900

    mailer生成

---
+++ front/src/styles/js/dictionary.ts
@@ -0,0 +1,12 @@
+import themes from "./themes"
+import { makeStyles } from "@material-ui/core/styles";
+
+const dictionaryStyles: any = {
+}
+
+export default dictionaryStyles
+
+export const useDictionaryStyles = makeStyles({
+  ...dictionaryStyles
+  }
+)
